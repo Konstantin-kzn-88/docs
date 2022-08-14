538ae4fc7641b679407748a7c32c5654e81fe434
konstantin <kuznetsovkm@yandex.ru>
2022-08-14 21:19:00 +0300

    MariaDB настроил подключение к серверу. Сделал подключение на REG.RU. VDS отвалился

--- connect_to_db/db_work.py
+++ connect_to_db/db_work.py
@@ -12,9 +12,11 @@ with mysql_conn as connection:
     cursor.execute("""DROP TABLE IF EXISTS objects""")
     cursor.execute("""CREATE TABLE objects(id INTEGER PRIMARY KEY, data TEXT NOT NULL,
                                             plan TEXT NOT NULL, name_plan TEXT NOT NULL)""")
-    # # SQL запрос на вставку BLOB
+    # # SQL запрос на вставку
     query = f"INSERT INTO objects (id, data,plan,name_plan) VALUES ('{1}', '{str(2)}', '{str(3)}', '{str(4)}')"
-
     cursor.execute(query)
+    query = f"INSERT INTO objects (id, data,plan,name_plan) VALUES ('{2}', '{str(2)}', '{str(3)}', '{str(4)}')"
+    cursor.execute(query)
+
     connection.commit()
     connection.close()
