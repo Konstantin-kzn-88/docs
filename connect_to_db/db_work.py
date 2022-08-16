ce6d4c173beec2674f3d69d0fb28886b05affea7
konstantin <kuznetsovkm@yandex.ru>
2022-08-16 20:32:22 +0300

    Основная информация по проету в виде таблиц БД.

--- connect_to_db/db_work.py
+++ connect_to_db/db_work.py
@@ -33,6 +33,7 @@ with mysql_conn as connection:
     # 3 Проекты
     cursor.execute("""CREATE TABLE Projects(Id INT PRIMARY KEY AUTO_INCREMENT, ObjectsId INT NOT NULL,
                                               Name_project VARCHAR(200) NOT NULL, Project_code VARCHAR(20) NOT NULL,
+                                              Project_description TEXT NOT NULL, Аutomation TEXT NOT NULL,
                                               CONSTRAINT projects_objects_fk FOREIGN KEY (ObjectsId)
                                               REFERENCES Objects (Id) ON DELETE CASCADE)""")
 
@@ -40,7 +41,27 @@ with mysql_conn as connection:
     cursor.execute("""CREATE TABLE Documents(Id INT PRIMARY KEY AUTO_INCREMENT, ProjectsId INT NOT NULL,
                                               Section_other_documentation VARCHAR(50) NOT NULL,
                                               Part_other_documentation_dpb VARCHAR(50) NOT NULL,
-                                              Part_other_documentation_gochs VARCHAR(50) NOT NULL)""")
+                                              Part_other_documentation_gochs VARCHAR(50) NOT NULL,
+                                              Book_dpb VARCHAR(50) NOT NULL,
+                                              Code_dpb VARCHAR(10) NOT NULL,
+                                              Tom_dpb VARCHAR(10) NOT NULL,
+                                              Book_rpz VARCHAR(50) NOT NULL,
+                                              Code_rpz VARCHAR(10) NOT NULL,
+                                              Tom_rpz VARCHAR(10) NOT NULL,
+                                              Book_ifl VARCHAR(50) NOT NULL,
+                                              Code_ifl VARCHAR(10) NOT NULL,
+                                              Tom_ifl VARCHAR(10) NOT NULL,
+                                              Book_gochs VARCHAR(50) NOT NULL,
+                                              Code_gochs VARCHAR(10) NOT NULL,
+                                              Tom_gochs VARCHAR(10) NOT NULL,
+                                              Section_fire_safety VARCHAR(50) NOT NULL,
+                                              Code_fire_safety VARCHAR(10) NOT NULL,
+                                              Tom_fire_safety VARCHAR(10) NOT NULL,
+                                              CONSTRAINT documents_projects_fk FOREIGN KEY (ProjectsId)
+                                              REFERENCES Projects (Id) ON DELETE CASCADE)""")
+
+
+
     # vvvvvvvvv___Заполнение_таблиц__vvvvvvvvvvvv
     #  SQL запрос на вставку
     # 1. Организации
@@ -77,25 +98,83 @@ with mysql_conn as connection:
     cursor.execute(query4, val4)
 
     # 3. Проекты
-    query5 = f"INSERT INTO Projects (ObjectsId, Name_project, Project_code) VALUES (%s, %s, %s)"
+    query5 = f"INSERT INTO Projects (ObjectsId, Name_project, Project_code, Project_description, Аutomation) " \
+             f"VALUES (%s, %s, %s, %s, %s)"
     val5 = (1, "Строительство нефтесборного трубопровода от МНС-644 Максимкинского нефтяного "
                "месторождения до МНС-645 с СПН-250 Фомкинского нефтяного месторождения",
-            "65-20")
+            "65-20", "Проектом предусмотрено...", "Проектом предусмотрена автоматизация...")
     cursor.execute(query5, val5)
 
-    query6 = f"INSERT INTO Projects (ObjectsId, Name_project, Project_code) VALUES (%s, %s, %s)"
+    query6 = f"INSERT INTO Projects (ObjectsId, Name_project, Project_code, Project_description, Аutomation) " \
+             f"VALUES (%s, %s, %s, %s, %s)"
     val6 = (2, "Обустройство куста скважин К-Д8 Демкинского нефтяного месторождения АО «Татех",
-            "70-20")
+            "70-20", "Проектом предусмотрено...", "Проектом предусмотрена автоматизация...")
     cursor.execute(query6, val6)
 
     # 4. Документы
-    query7 = f"INSERT INTO Documents (ProjectsId, Section_other_documentation, Part_other_documentation_dpb, Part_other_documentation_gochs) VALUES (%s, %s, %s, %s)"
+    query7 = f"INSERT INTO Documents " \
+             f"(ProjectsId, " \
+             f"Section_other_documentation, " \
+             f"Part_other_documentation_dpb, " \
+             f"Part_other_documentation_gochs, " \
+             f"Book_dpb, Code_dpb, Tom_dpb, " \
+             f"Book_rpz, Code_rpz, Tom_rpz, " \
+             f"Book_ifl, Code_ifl, Tom_ifl, " \
+             f"Book_gochs, Code_gochs, Tom_gochs, " \
+             f"Section_fire_safety, " \
+             f"Code_fire_safety, Tom_fire_safety) " \
+             f"VALUES " \
+             f"(%s, %s, %s, " \
+             f"%s, %s, %s, " \
+             f"%s, %s, %s, " \
+             f"%s, %s, %s, " \
+             f"%s, %s, %s, " \
+             f"%s, %s, %s, " \
+             f"%s)"
+
     val7 = (1,
             "Раздел 12 «Иная документация в случаях, предусмотренных федеральными законами»",
             "Часть 1. Декларация промышленной безопасности",
-            "Часть 2. «Перечень мероприятий по гражданской обороне»")
+            "Часть 2. «Перечень мероприятий по гражданской обороне»",
+            "Книга 1. Декларация промышленной безопасности", "ДПБ1", "12.1.1",
+            "Книга 2. Расчентно-пояснительная записка", "ДПБ2", "12.1.2",
+            "Книга 3. Информационный лист", "ДПБ3", "12.1.3",
+            "", "ГОЧС", "12.2",
+            "Раздел 9 «Мероприятия по обеспечению пожарной безопасности»", "ПБ", "9")
 
     cursor.execute(query7, val7)
+
+    query8 = f"INSERT INTO Documents " \
+             f"(ProjectsId, " \
+             f"Section_other_documentation, " \
+             f"Part_other_documentation_dpb, " \
+             f"Part_other_documentation_gochs, " \
+             f"Book_dpb, Code_dpb, Tom_dpb, " \
+             f"Book_rpz, Code_rpz, Tom_rpz, " \
+             f"Book_ifl, Code_ifl, Tom_ifl, " \
+             f"Book_gochs, Code_gochs, Tom_gochs, " \
+             f"Section_fire_safety, " \
+             f"Code_fire_safety, Tom_fire_safety) " \
+             f"VALUES " \
+             f"(%s, %s, %s, " \
+             f"%s, %s, %s, " \
+             f"%s, %s, %s, " \
+             f"%s, %s, %s, " \
+             f"%s, %s, %s, " \
+             f"%s, %s, %s, " \
+             f"%s)"
+
+    val8 = (1,
+            "Раздел 10 «Иная документация в случаях, предусмотренных федеральными законами»",
+            "Часть 1. Декларация промышленной безопасности",
+            "Часть 2. «Перечень мероприятий по гражданской обороне»",
+            "Книга 1. Декларация промышленной безопасности", "ДПБ1", "10.1.1",
+            "Книга 2. Расчентно-пояснительная записка", "ДПБ2", "10.1.2",
+            "Книга 3. Информационный лист", "ДПБ3", "10.1.3",
+            "", "ГОЧС", "10.2",
+            "Раздел 9 «Мероприятия по обеспечению пожарной безопасности»", "ПБ", "9")
+
+    cursor.execute(query8, val8)
 
     connection.commit()
     connection.close()
