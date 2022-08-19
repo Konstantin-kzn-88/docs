54986874a259da4983cc397416fd4095a8953b4e
Konstantin <kuznetsovkm@yandex.ru>
2022-08-19 16:59:40 +0300

    Таблицы + начало запрет редактирования + переименование колонок таблиц

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 import os
 from pathlib import Path
-from PySide2.QtSql import QSqlDatabase, QSqlQuery, QSqlRelationalTableModel
-from PySide2.QtWidgets import QApplication, QMainWindow, QComboBox, QMessageBox, QWidget, QGridLayout, QFormLayout, QGroupBox, QTableView, QStyleFactory
+from PySide2.QtSql import QSqlDatabase, QSqlQuery, QSqlRelationalTableModel, QSqlRelation
+from PySide2.QtWidgets import QApplication, QMainWindow, QComboBox, QMessageBox, QWidget, QGridLayout, \
+    QFormLayout, QGroupBox, QTableView, QStyleFactory, QStyledItemDelegate
 from PySide2.QtGui import QIcon
-
-
-
+from PySide2.QtCore import Qt
+from itertools import count
 
 db = QSqlDatabase.addDatabase("QMYSQL")
 db.setHostName("server167.hosting.reg.ru")
@@ -15,19 +15,53 @@ db.setUserName("u1082920_qt_cl")
 db.setPassword("1201kZn1201!")
 db.open()
 
-
 if db.open():
     print("Database Open!")
 else:
     print("Database NOT Open!")
 
 
-
+class ReadOnlyDelegate(QStyledItemDelegate):
+    def createEditor(self, parent, option, insex):
+        # print(f'parent=`{parent}`, \noption=`{option}`, \ninsex=`{insex}`\n')
+        return
 
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None) -> None:
         super().__init__()
+        self.table_list = ["Организации", "Объекты", "Проекты", "Документы", "Вещества", "Оборудование", "Трубопроводы"]
+        self.table_list_eng = ["Organizations", "Objects", "Projects", "Documents", "Substances", "Devices",
+                               "Pipelines"]
+        self.field_dict = {
+            "Organizations": (
+                'Организация', 'Полное наименование', 'Должноть руководителя', 'Ф.И.О. руководителя', 'Должность тех.руководителя', 'Ф.И.О. тех.руководителя',
+                'Юр. адрес',
+                'Телефон', 'Факс', 'Email', 'Лицензия', 'Дата получения лицензии'),
+            "Objects":
+                ('Организация', 'Наименование объекта', 'Адрес объекта', 'Рег. №', 'Класс опасности'),
+            "Projects":
+                ('ObjectsId', 'Name_project', 'Project_code', 'Project_description', 'Аutomation'),
+            "Documents":
+                ('ProjectsId', 'Section_other_documentation', 'Part_other_documentation_dpb',
+                 'Part_other_documentation_gochs',
+                 'Book_dpb', 'Code_dpb', 'Tom_dpb', 'Book_rpz', 'Code_rpz', 'Tom_rpz', 'Book_ifl', 'Code_ifl',
+                 'Tom_ifl',
+                 'Book_gochs', 'Code_gochs', 'Tom_gochs', 'Section_fire_safety', 'Code_fire_safety', 'Tom_fire_safety'),
+            "Substances":
+                ('Name_sub', 'Density', 'Density_gas', 'Molecular_weight', 'Steam_pressure', 'Flash_temperature',
+                 'Boiling_temperature', 'Class_substance', 'Heat_of_combustion', 'Sigma', 'Energy_level',
+                 'Lower_concentration', 'Cost'),
+            "Devices":
+                ('ProjectsId', 'SubId', 'Type_device', 'Pozition', 'Name', 'Locations', 'Material', 'Ground', 'Target',
+                 'Volume', 'Completion', 'Pressure', 'Temperature', 'Spill_square', 'View_space', 'Death_person',
+                 'Injured_person', 'Time_person'),
+            "Pipelines":
+                ('ProjectsId', 'SubId', 'Pozition', 'Name', 'Locations', 'Material', 'Ground', 'Target',
+                 'Length', 'Diameter', 'Pressure', 'Temperature', 'Flow', 'View_space', 'Death_person',
+                 'Injured_person', 'Time_person')
+        }
+
         self.set_ico()
         self.init_UI()
 
@@ -41,24 +75,31 @@ class MainWindow(QMainWindow):
         central_grid = QGridLayout(self)
         central_grid.setColumnStretch(0, 7)
         central_grid.setColumnStretch(1, 1)
-        # ___________1_START________________
+        # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
         # Рамка
         layout_select = QFormLayout(self)
         GB_select = QGroupBox('Выбор')
         GB_select.setStyleSheet("QGroupBox { font-weight : bold; }")
-        self.organisation_list = QComboBox()
-        layout_select.addRow("", self.organisation_list)
+        self.select_table = QComboBox()
+        self.select_table.addItems(self.table_list)
+        self.select_table.currentIndexChanged.connect(self.show_table)
+        layout_select.addRow("", self.select_table)
         GB_select.setLayout(layout_select)
 
-        # ___________2_START________________
+        # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
         # Рамка
         layout_table = QFormLayout(self)
-        GB_table = QGroupBox('Выбор')
+        GB_table = QGroupBox('Данные')
         GB_table.setStyleSheet("QGroupBox { font-weight : bold; }")
         self.table = QTableView()
         self.model = QSqlRelationalTableModel(db=db)
         self.table.setModel(self.model)
+        # Делаем не редактируемым первый столбец с id
+        self.delegate = ReadOnlyDelegate(self.table)
+        # self.table.setItemDelegateForColumn(0, self.delegate)
+
         self.model.setTable("Organizations")
+
         self.model.select()
         layout_table.addRow("", self.table)
         GB_table.setLayout(layout_table)
@@ -80,18 +121,47 @@ class MainWindow(QMainWindow):
 
         self.show()
 
+    def show_table(self, index):
+        self.table.setModel(self.model)
+        self.model.setTable(self.table_list_eng[index])
+
+        if index in (0, 4):
+            self.table.setItemDelegateForColumn(0, self.delegate)
+            self.table.setItemDelegateForColumn(1, None)
+            self.table.setItemDelegateForColumn(2, None)
+            self.table.setItemDelegateForColumn(3, None)
+        elif index in (1, 2, 3):
+            self.table.setItemDelegateForColumn(0, self.delegate)
+            self.table.setItemDelegateForColumn(1, self.delegate)
+        elif index in (5, 6):
+            self.table.setItemDelegateForColumn(0, self.delegate)
+            self.table.setItemDelegateForColumn(1, self.delegate)
+            self.table.setItemDelegateForColumn(2, self.delegate)
+
+        if index == 0:
+            for i in count(1,1):
+                if i > len(self.field_dict["Organizations"]): break
+                self.model.setHeaderData(i, Qt.Horizontal, self.field_dict["Organizations"][i-1])
+
+        if index == 1:
+            self.model.setRelation(1, QSqlRelation("Organizations", "Id", "Name_org"))
+            for i in count(1,1):
+                if i > len(self.field_dict["Objects"]): break
+                self.model.setHeaderData(i, Qt.Horizontal, self.field_dict["Objects"][i-1])
+
+
+        self.model.select()
+
     def set_ico(self):
         path_ico = str(Path(os.getcwd()))
         print(path_ico)
         self.main_ico = QIcon(path_ico + '/ico/main.png')
         self.setWindowIcon(self.main_ico)
 
-
-
     def closeEvent(self, event):
-        reply = QMessageBox.question\
-        (self, 'Выход из программы',
-            "Вы уверены, что хотите уйти?",
+        reply = QMessageBox.question \
+            (self, 'Выход из программы',
+             "Вы уверены, что хотите уйти?",
              QMessageBox.Yes,
              QMessageBox.No)
         if reply == QMessageBox.Yes:
@@ -99,6 +169,7 @@ class MainWindow(QMainWindow):
             event.accept()
         else:
             event.ignore()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
