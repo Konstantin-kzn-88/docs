ef2fba0802803191872b5a9cfbc9b66f85bc742a
konstantin <kuznetsovkm@yandex.ru>
2022-09-09 16:07:28 +0300

    Дерево событий РТН + тесты

--- risk/risk_event_tree.py
+++ risk/risk_event_tree.py
@@ -37,10 +37,10 @@ class Event_tree:
         else:
             a, b, c = ratio_tuple[i]
 
-        return ("{:.2e}".format(probability * a),                       # пожар
-                "{:.2e}".format(probability * (1 - a) * b * c),         # взрыв
-                "{:.2e}".format(probability * (1 - a) * b * (1 - c)),   # вспышка
-                "{:.2e}".format(probability * (1 - a) * (1 - b)))       # рассеивание
+        return ("{:.2e}".format(probability * a),  # пожар
+                "{:.2e}".format(probability * (1 - a) * b * c),  # взрыв
+                "{:.2e}".format(probability * (1 - a) * b * (1 - c)),  # вспышка
+                "{:.2e}".format(probability * (1 - a) * (1 - b)))  # рассеивание
 
     @staticmethod
     def mchs_gas(flow_rate: float, probability: float) -> tuple:
@@ -73,58 +73,59 @@ class Event_tree:
         else:
             a, b, c = ratio_tuple[1]
 
-        return ("{:.2e}".format(probability * a),                       # факел
-                "{:.2e}".format(probability * (1 - a) * b * c),         # взрыв
-                "{:.2e}".format(probability * (1 - a) * b * (1 - c)),   # вспышка
-                "{:.2e}".format(probability * (1 - a) * (1 - b)))       # рассеивание
+        return ("{:.2e}".format(probability * a),  # факел
+                "{:.2e}".format(probability * (1 - a) * b * c),  # взрыв
+                "{:.2e}".format(probability * (1 - a) * b * (1 - c)),  # вспышка
+                "{:.2e}".format(probability * (1 - a) * (1 - b)))  # рассеивание
 
     @staticmethod
-    def rostech_liquid_with_pressure(type_obj: int):
+    def rostech_liquid(type_obj: int, flow_rate: float, probability: float):
         """
         "Дерево собьтий" для жидкости в емкости под давлением по приказу РТН от 17.08.2015 N 317
 
         Parametrs:
+        :param flow_rate: аварийный расход, кг/с (0 - полняй разрыв)
+        :param probability: частота первичного события, 1/год (например probability = 20)
         :param type_obj: тип объекта, -
 
-        Тип оборудования:
+        Тип объекта:
 
         0 - трубопровод
-        1 - емкость под давлением
-        2 - насос герметичный
-        3 - колонны конденсаторы фильтры
-        4 - резервуар хранения
-        5 - теплообменники
-        6 - цистерны
-
+        1 - емкость, резервуар
 
         Return:
         probability scenarios
         """
         ratio_tuple = (
-            (0.2, 0.24, 0.6),
-            (0.005, 0.005, 0.08),
-            (0.035, 0.036, 0.24),
-            (0.15, 0.176, 0.6)
+            (0.05, 0.061, 0.2),
+            (0.005, 0.005, 0.2),
+            (0.04, 0.042, 0.2),
+            (0.15, 0.176, 0.6),
+            (0.05, 0.05, 0.2)
         )
 
-        if flow_rate == 0:  # gap (разрыв)
-            a, b, c = ratio_tuple[0]
-        elif flow_rate <= 0.5:
-            a, b, c = ratio_tuple[1]
-        elif 0.5 < flow_rate <= 10:
-            a, b, c = ratio_tuple[2]
-        elif flow_rate > 10:  # очень большой расход
-            a, b, c = ratio_tuple[3]
-        else:
-            a, b, c = ratio_tuple[1]
+        if type_obj == 0:
+            if flow_rate == 0:  # gap (разрыв)
+                a, b, c = ratio_tuple[0]
+            elif flow_rate <= 1:
+                a, b, c = ratio_tuple[1]
+            elif 1 < flow_rate <= 50:
+                a, b, c = ratio_tuple[2]
+            elif flow_rate > 50:  # очень большой расход
+                a, b, c = ratio_tuple[3]
+            else:
+                a, b, c = ratio_tuple[1]
+        else: # емкостное и пр. оборудование
+            a, b, c = ratio_tuple[4]
 
-        return ("{:.2e}".format(probability * a),                       # факел
-                "{:.2e}".format(probability * (1 - a) * b * c),         # взрыв
-                "{:.2e}".format(probability * (1 - a) * b * (1 - c)),   # вспышка
-                "{:.2e}".format(probability * (1 - a) * (1 - b)))       # рассеивание
+        return ("{:.2e}".format(probability * a),  # пожар
+                "{:.2e}".format(probability * (1 - a) * b * c),  # взрыв
+                "{:.2e}".format(probability * (1 - a) * b * (1 - c)),  # вспышка
+                "{:.2e}".format(probability * (1 - a) * (1 - b)))  # рассеивание
+
 
 if __name__ == '__main__':
-    print(Event_tree.mchs_gas(0, 0.000007))
-    print(Event_tree.mchs_gas(0.3, 0.000007))
-    print(Event_tree.mchs_gas(6, 0.000007))
-    print(Event_tree.mchs_gas(11, 0.000007))
+    print(Event_tree.rostech_liquid(0, 0, 0.000007))
+    print(Event_tree.rostech_liquid(0, 2, 0.000007))
+    print(Event_tree.rostech_liquid(0, 52, 0.000007))
+    print(Event_tree.rostech_liquid(1, 0, 0.000007))
--- tests/test_risk.py
+++ tests/test_risk.py
@@ -53,6 +53,28 @@ class ServerTest(TestCase):
             self.assertEqual(
                 len(risk_event_tree.Event_tree().mchs_gas(flow_rate=f / 10, probability=7e-5)), 4)
 
+    def test_risk_event_tree_rostech(self):
+        # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+        # mchs_liquid
+        self.assertEqual(
+            risk_event_tree.Event_tree().rostech_liquid(type_obj=0, flow_rate=0, probability=0.000007),
+            ('3.50e-07', '8.11e-08', '3.25e-07', '6.24e-06'))
+        self.assertEqual(
+            risk_event_tree.Event_tree().rostech_liquid(type_obj=0, flow_rate=2, probability=0.000007),
+            ('2.80e-07', '5.64e-08', '2.26e-07', '6.44e-06'))
+        self.assertEqual(
+            risk_event_tree.Event_tree().rostech_liquid(type_obj=0, flow_rate=52, probability=0.000007),
+            ('1.05e-06', '6.28e-07', '4.19e-07', '4.90e-06'))
+        self.assertEqual(
+            risk_event_tree.Event_tree().rostech_liquid(type_obj=1, flow_rate=0, probability=0.000007),
+            ('3.50e-07', '6.65e-08', '2.66e-07', '6.32e-06'))
+
+        # mchs_liquid_len
+        for f in range(0, 1000, 5):
+            self.assertEqual(
+                len(risk_event_tree.Event_tree().rostech_liquid(type_obj=random.choice((1, 2)), flow_rate=f / 10,
+                                                                probability=7e-5)), 4)
+
 
 if __name__ == '__main__':
     main()
