aa66f99e9e27a0d3e0304f341f1f72f377547f2c
Konstantin_user <kuznetsovkm@yandex.ru>
2022-09-09 12:19:11 +0300

    ...

--- risk/risk_event_tree.py
+++ risk/risk_event_tree.py
@@ -1,6 +1,6 @@
 class Event_tree:
     @staticmethod
-    def mchs(flash_temperature: float, flow_rate: float, probability: float) -> tuple:
+    def mchs_liquid(flash_temperature: float, flow_rate: float, probability: float) -> tuple:
         """
         "Дерево собьтий" для ЛВЖ и ГЖ по приказу МЧС РФ от 10.07.2009 N 404
         "Об утверждении методики определения расчетных величин пожарного риска на производственных объектах"
@@ -37,10 +37,94 @@ class Event_tree:
         else:
             a, b, c = ratio_tuple[i]
 
-        return ("{:.2e}".format(probability * a),
-                "{:.2e}".format(probability * (1 - a) * b * c),
-                "{:.2e}".format(probability * (1 - a) * b * (1 - c)),
-                "{:.2e}".format(probability * (1 - a) * (1 - b)))
+        return ("{:.2e}".format(probability * a),                       # пожар
+                "{:.2e}".format(probability * (1 - a) * b * c),         # взрыв
+                "{:.2e}".format(probability * (1 - a) * b * (1 - c)),   # вспышка
+                "{:.2e}".format(probability * (1 - a) * (1 - b)))       # рассеивание
+
+    @staticmethod
+    def mchs_gas(flow_rate: float, probability: float) -> tuple:
+        """
+        "Дерево собьтий" для газа по приказу МЧС РФ от 10.07.2009 N 404
+        "Об утверждении методики определения расчетных величин пожарного риска на производственных объектах"
+
+        Parametrs:
+        :param flow_rate: аварийный расход, кг/с (0 - полняй разрыв)
+        :param probability: частота первичного события, 1/год (например probability = 20)
+
+        Return:
+        probability scenarios
+        """
+        ratio_tuple = (
+            (0.2, 0.24, 0.6),
+            (0.005, 0.005, 0.08),
+            (0.035, 0.036, 0.24),
+            (0.15, 0.176, 0.6)
+        )
+
+        if flow_rate == 0:  # gap (разрыв)
+            a, b, c = ratio_tuple[0]
+        elif flow_rate <= 0.5:
+            a, b, c = ratio_tuple[1]
+        elif 0.5 < flow_rate <= 10:
+            a, b, c = ratio_tuple[2]
+        elif flow_rate > 10:  # очень большой расход
+            a, b, c = ratio_tuple[3]
+        else:
+            a, b, c = ratio_tuple[1]
+
+        return ("{:.2e}".format(probability * a),                       # факел
+                "{:.2e}".format(probability * (1 - a) * b * c),         # взрыв
+                "{:.2e}".format(probability * (1 - a) * b * (1 - c)),   # вспышка
+                "{:.2e}".format(probability * (1 - a) * (1 - b)))       # рассеивание
+
+    @staticmethod
+    def rostech_liquid_with_pressure(type_obj: int):
+        """
+        "Дерево собьтий" для жидкости в емкости под давлением по приказу РТН от 17.08.2015 N 317
+
+        Parametrs:
+        :param type_obj: тип объекта, -
+
+        Тип оборудования:
+
+        0 - трубопровод
+        1 - емкость под давлением
+        2 - насос герметичный
+        3 - колонны конденсаторы фильтры
+        4 - резервуар хранения
+        5 - теплообменники
+        6 - цистерны
+
+
+        Return:
+        probability scenarios
+        """
+        ratio_tuple = (
+            (0.2, 0.24, 0.6),
+            (0.005, 0.005, 0.08),
+            (0.035, 0.036, 0.24),
+            (0.15, 0.176, 0.6)
+        )
+
+        if flow_rate == 0:  # gap (разрыв)
+            a, b, c = ratio_tuple[0]
+        elif flow_rate <= 0.5:
+            a, b, c = ratio_tuple[1]
+        elif 0.5 < flow_rate <= 10:
+            a, b, c = ratio_tuple[2]
+        elif flow_rate > 10:  # очень большой расход
+            a, b, c = ratio_tuple[3]
+        else:
+            a, b, c = ratio_tuple[1]
+
+        return ("{:.2e}".format(probability * a),                       # факел
+                "{:.2e}".format(probability * (1 - a) * b * c),         # взрыв
+                "{:.2e}".format(probability * (1 - a) * b * (1 - c)),   # вспышка
+                "{:.2e}".format(probability * (1 - a) * (1 - b)))       # рассеивание
 
 if __name__ == '__main__':
-    print(Event_tree.mchs(30, 0.3, 0.000007))
+    print(Event_tree.mchs_gas(0, 0.000007))
+    print(Event_tree.mchs_gas(0.3, 0.000007))
+    print(Event_tree.mchs_gas(6, 0.000007))
+    print(Event_tree.mchs_gas(11, 0.000007))
--- tests/test_risk.py
+++ tests/test_risk.py
@@ -6,24 +6,52 @@ from risk import risk_event_tree
 class ServerTest(TestCase):
 
     def test_risk_event_tree_mchs(self):
-        self.assertEqual(risk_event_tree.Event_tree().mchs(flash_temperature=10, flow_rate=0, probability=0.000007),
-                         ('1.40e-06', '8.06e-07', '5.38e-07', '4.26e-06'))
-        self.assertEqual(risk_event_tree.Event_tree().mchs(flash_temperature=10, flow_rate=1, probability=0.000007),
-                         ('2.45e-07', '5.84e-08', '1.85e-07', '6.51e-06'))
-        self.assertEqual(risk_event_tree.Event_tree().mchs(flash_temperature=10, flow_rate=0.3, probability=0.000007),
-                         ('3.50e-08', '2.79e-09', '3.20e-08', '6.93e-06'))
-        self.assertEqual(risk_event_tree.Event_tree().mchs(flash_temperature=30, flow_rate=0, probability=0.000007),
-                         ('3.50e-07', '4.06e-08', '3.65e-07', '6.24e-06'))
-        self.assertEqual(risk_event_tree.Event_tree().mchs(flash_temperature=30, flow_rate=1, probability=0.000007),
-                         ('1.05e-07', '5.17e-09', '9.83e-08', '6.79e-06'))
-        self.assertEqual(risk_event_tree.Event_tree().mchs(flash_temperature=30, flow_rate=0.3, probability=0.000007),
-                         ('3.50e-08', '1.74e-09', '3.31e-08', '6.93e-06'))
+        # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+        # mchs_liquid
+        self.assertEqual(
+            risk_event_tree.Event_tree().mchs_liquid(flash_temperature=10, flow_rate=0, probability=0.000007),
+            ('1.40e-06', '8.06e-07', '5.38e-07', '4.26e-06'))
+        self.assertEqual(
+            risk_event_tree.Event_tree().mchs_liquid(flash_temperature=10, flow_rate=1, probability=0.000007),
+            ('2.45e-07', '5.84e-08', '1.85e-07', '6.51e-06'))
+        self.assertEqual(
+            risk_event_tree.Event_tree().mchs_liquid(flash_temperature=10, flow_rate=0.3, probability=0.000007),
+            ('3.50e-08', '2.79e-09', '3.20e-08', '6.93e-06'))
+        self.assertEqual(
+            risk_event_tree.Event_tree().mchs_liquid(flash_temperature=30, flow_rate=0, probability=0.000007),
+            ('3.50e-07', '4.06e-08', '3.65e-07', '6.24e-06'))
+        self.assertEqual(
+            risk_event_tree.Event_tree().mchs_liquid(flash_temperature=30, flow_rate=1, probability=0.000007),
+            ('1.05e-07', '5.17e-09', '9.83e-08', '6.79e-06'))
+        self.assertEqual(
+            risk_event_tree.Event_tree().mchs_liquid(flash_temperature=30, flow_rate=0.3, probability=0.000007),
+            ('3.50e-08', '1.74e-09', '3.31e-08', '6.93e-06'))
 
-    def test_len_risk_event_tree_mchs(self):
+        # mchs_liquid_len
         for t in range(-20, 30, 1):
             for f in range(0, 100, 1):
                 self.assertEqual(
-                    len(risk_event_tree.Event_tree().mchs(flash_temperature=t, flow_rate=f / 10, probability=7e-5)), 4)
+                    len(risk_event_tree.Event_tree().mchs_liquid(flash_temperature=t, flow_rate=f / 10,
+                                                                 probability=7e-5)), 4)
+        # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+        # mchs_gas
+        self.assertEqual(
+            risk_event_tree.Event_tree().mchs_gas(flow_rate=0, probability=0.000007),
+            ('1.40e-06', '8.06e-07', '5.38e-07', '4.26e-06'))
+        self.assertEqual(
+            risk_event_tree.Event_tree().mchs_gas(flow_rate=0.3, probability=0.000007),
+            ('3.50e-08', '2.79e-09', '3.20e-08', '6.93e-06'))
+        self.assertEqual(
+            risk_event_tree.Event_tree().mchs_gas(flow_rate=6, probability=0.000007),
+            ('2.45e-07', '5.84e-08', '1.85e-07', '6.51e-06'))
+        self.assertEqual(
+            risk_event_tree.Event_tree().mchs_gas(flow_rate=11, probability=0.000007),
+            ('1.05e-06', '6.28e-07', '4.19e-07', '4.90e-06'))
+
+        # mchs_gas_len
+        for f in range(0, 100, 1):
+            self.assertEqual(
+                len(risk_event_tree.Event_tree().mchs_gas(flow_rate=f / 10, probability=7e-5)), 4)
 
 
 if __name__ == '__main__':
