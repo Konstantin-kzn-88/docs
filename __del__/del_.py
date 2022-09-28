56d76f28841587a97ba475356d8f492d238cc394
Konstantin <kuznetsovkm@yandex.ru>
2022-09-28 11:28:50 +0300

    РПЗ готов шаблон

--- __del__/del_.py
+++ __del__/del_.py
@@ -1,20 +1,12 @@
-print('Программа позволяет узнать, сколько раз \nв заданном диапазоне встречается выбранное число.\n')
+# a, b, c = map(int, input().split())
 
-num_start = int(input('Введите начальное число диапазона: '))
-num_finish = int(input('Введите конечное число диапазона (не более 1 миллиона): '))
-num_look = input('Задайте однозначное число, которое необходимо посчитать: ')
+a = int(input())
+b = int(input())
+c = int(input())
 
-if len(num_look) == 1:
-    x = str(list(range(num_start, num_finish + 1))).count(str(num_look))
+if a > b and a > c:
+    print(a)
+elif b > a and b > c:
+    print(b)
 else:
-    x = 0
-    range_in_str = str(list(range(num_start, num_finish + 1)))  # строка поиска из заданного диапазона
-    len_slice = len(num_look)                                   # длина среза для поиска
-
-    for i in range(len(range_in_str)):
-        print(f'num_look = {num_look}, slice = {range_in_str[i:i + len_slice]}')
-        if range_in_str[i:i + len_slice] in num_look:
-            x += 1
-
-print('В последовательности чисел от ', num_start, ' до ', num_finish, ' цифра: ', num_look, 'встречается ', x,
-      " раз(а)")
+    print(c)
