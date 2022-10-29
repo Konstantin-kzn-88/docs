34d2025caa7737564ca0e1748d3bbf355b1bbaca
Konstantin <kuznetsovkm@yandex.ru>
2022-10-29 15:31:00 +0300

    Добавил истечение газа (по амер.методике)

--- calc/calc_gas_outflow.py
+++ calc/calc_gas_outflow.py
@@ -11,13 +11,14 @@
 import math
 
 TEMP_TO_KELVIN = 273
+TEMP_TO_C = -273
 KMOL_TO_MOL = 0.001
 R = 8.413  # газовая постоянная
 PRESSURE_ATM = 0.101325  # атмосферное давление, МПа
 DISCHARGE = 0.62  # коэф.истечения, допускается брать 0.62 (p.37)
 MPA_TO_PA = math.pow(10, 6)  # МПа в Па
 PA_TO_MPA = math.pow(10, -6)  # Па в МПа
-CP = 0.01 # теплоемкость при постоянном объеме
+CP = 0.01  # теплоемкость при постоянном объеме
 
 
 class Outflow:
@@ -66,13 +67,36 @@ class Outflow:
             return self.poisson_ratio * math.pow(i, j)
 
     def flow_rate_init(self, hole_diameter: float, pressure: float, temperature: int):
+        '''
+        Функция расчета начального значения расхода
+        :param hole_diameter: - диаметр отверстия, мм
+        :param pressure: - давление, МПа
+        :param temperature: - температура, град. K
+        :return: расход, кг/с
+        '''
         square = (1 / 4) * math.pi * math.pow(hole_diameter, 2)
         k = self.coefficient_K()
         a = DISCHARGE * square * pressure * MPA_TO_PA * k
-        b = math.pow(self.mol_weight / (self.poisson_ratio * R * temperature), 1 / 2)
+        b = math.pow(math.fabs(self.mol_weight / (self.poisson_ratio * R * temperature)), 1 / 2)
         return float(a * b)
 
     def result(self, hole_diameter: float):
+        '''
+        Функция предоставления данных для аварийного расхода из оборудования
+
+        :param hole_diameter: - диаметр отверстия, м
+        :return:
+        weight - масса газа, оставшаяся на момент time, кг
+        time - время измерения аварийного расхода, с
+        temperature - температура газа, град.С
+        density_gas - плотность газа, кг/м3
+        pressure - давление в аварийном оборудовании, МПа
+        mass_flow_rate - аварийный расход через отверстие, кг/с
+        delta_density - изменение плотности по времени
+        delta_temperature - изменение температуры по времени
+        '''
+
+
         weight = []  # кг
         time = []  # с
         temperature = []  # град.С
@@ -86,34 +110,48 @@ class Outflow:
         while True:
             time.append(t)
             if t == 0:
+                # Температура, давление, плотность
                 temperature.append(self.temperature)
                 pressure.append(self.pressure)
-                density_gas.append(round(self.density(),2))
-                po = round(self.flow_rate_init(hole_diameter, self.pressure, self.temperature),2)
+                density_gas.append(round(self.density(), 2))
+                # Расход
+                po = round(self.flow_rate_init(hole_diameter, self.pressure, self.temperature), 2)
                 mass_flow_rate.append(po)
-                d_po = round(-po / self.volume,2)
-                delta_density.append(d_po)
-                d_t = round((self.pressure / (math.pow(density_gas[0], 2) * CP)) * delta_density[0],2)
+                # Изменение плотности, температуры по времении
+                d_p = round(-po / self.volume, 2)
+                delta_density.append(d_p)
+                d_t = round((self.pressure / (math.pow(density_gas[0], 2) * CP)) * delta_density[0], 2)
                 delta_temperature.append(d_t)
+                # Масса газа в оборудовании
+                weight.append(round(self.volume*self.density(),0))
             else:
-                temp = round(temperature[t-1] + delta_temperature[t-1],2)
-                den = round(density_gas[t-1] + delta_density[t-1],2)
-                pres = round(R*temp*(den/self.mol_weight)*PA_TO_MPA,2)
+                # Температура, давление, плотность
+                temp = round(temperature[t - 1] + delta_temperature[t - 1], 2)
+                den = round(density_gas[t - 1] + delta_density[t - 1], 2)
+                pres = round(R * temp * (den / self.mol_weight) * PA_TO_MPA, 2)
 
                 temperature.append(temp)
                 pressure.append(pres)
                 density_gas.append(den)
-
-                po = round(self.flow_rate_init(hole_diameter, pressure[t], temperature[t]),2)
+                # Расход
+                po = round(self.flow_rate_init(hole_diameter, pressure[t], temperature[t]), 2)
                 mass_flow_rate.append(po)
-                d_po = round(-po / self.volume,2)
-                delta_density.append(d_po)
-                d_t = round((self.pressure / (math.pow(density_gas[0], 2) * CP)) * delta_density[t],2)
+                # Изменение плотности, температуры по времении
+                d_p = round(-po / self.volume, 2)
+                delta_density.append(d_p)
+                d_t = round((self.pressure / (math.pow(density_gas[0], 2) * CP)) * delta_density[t], 2)
                 delta_temperature.append(d_t)
-
-
+                # Масса газа в оборудовании
+                weight.append(round(weight[t-1]-mass_flow_rate[t-1],0))
+            # Следущая секунда
             t += 1
-            if t > 5: break
+            # Проверки по времени, масса, давлению
+            if t > 70: break
+            if weight[-1] < 30: break
+            if pressure[-1] < 0.07: break
+            if weight[-1] < mass_flow_rate[0]: break
+        # Перевод температуры в град.С
+        temperature = [round(i + TEMP_TO_C,0) for i in temperature]
         return (weight, time, temperature, density_gas, pressure, mass_flow_rate, delta_density, delta_temperature)
 
 
@@ -121,5 +159,5 @@ if __name__ == '__main__':
     cls = Outflow(50, 5, 15, 2, 1.4)
     print(cls.density())
     print(cls.coefficient_K())
-    for i in cls.result(0.1):
+    for i in cls.result(0.2):
         print(i)
