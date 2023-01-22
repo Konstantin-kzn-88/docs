fca8150b17b4c97c098a36700440baaf0a75dc22
konstantin <kuznetsovkm@yandex.ru>
2023-01-22 11:40:24 +0300

    Исчтечение жидкости из трубопровода (начало теории)

---
+++ calc/calc_liguid_outflow_pipe.py
@@ -0,0 +1,14 @@
+# скорость жидкости в трубе, м/с
+# https://calculatorium.ru/physics/liquid-speed-in-the-pipeline
+# v = (4Q)/(pi*D^2) - где Q - расход, м3/ч; D - диаметр в мм
+
+# https://gidrotgv.ru/spravka-po-koefficientu-gidravlicheskogo-treniya-kalkulyatory-onlajn/
+# коэф.гидравлического трения
+
+
+# Re = v*D /mu - где v - скорость жидкости в трубе, м/с; D - диаметр в мм; mu - кинематическая вязкость м2/c (0.00006)
+# https://petrodigest.ru/terms/vyazkost-nefti
+
+# 1. Расход в точке за время отключения
+# 2. Расход в безнапорном режиме (за время приезда бригады)
+# 3. Условия перетока по учаасткам
